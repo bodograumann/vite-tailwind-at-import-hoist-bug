17d062ea82736be94ec31162021d97ff491d2007
Bodo Graumann <mail@bodograumann.de>
2022-01-12 18:08:41 +0100

    Use externally imported font from google fonts

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "IBM Plex Sans", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
